b6d5e62ef5263555efaf4584abeee57c7e68b7ef
luidimso <luidi_mso@outlook.com>
2021-07-22 21:08:15 -0300

    Implementando os testes unitarios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'trabalho-tdd'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    // expect(app.title).toEqual('trabalho-tdd');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('trabalho-tdd app is running!');
+    expect(compiled.querySelector('section h3').textContent).toContain('Trabalhadores afastados por doença:');
+  });
+
+  it(`should return a array`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(Array.isArray(app.getTrabalhadoresAfastadosPorDoenca())).toBe(true);
+  });
+
+  it(`should return at least one worker as total`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.getTotalDeTrabalhadores()).toBeGreaterThan(0);
+  });
+
+  it(`should not total away workers be greater than total workers`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.getTrabalhadoresAfastadosPorDoenca().length).toBeLessThanOrEqual(app.getTotalDeTrabalhadores());
   });
 });
